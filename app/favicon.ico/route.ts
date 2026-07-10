2dd777133282d85b7fc1ebc0bf3dbfef17d68d2d
Abbos <65133412+abbos-khamidov@users.noreply.github.com>
2026-07-10 20:20:38 +0500

    Update favicon artwork

--- app/favicon.ico/route.ts
+++ app/favicon.ico/route.ts
@@ -2,9 +2,8 @@ import sharp from "sharp";
 
 const svg = `
 <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64">
-  <rect width="64" height="64" rx="14" fill="#d8ff2c"/>
-  <path d="M19 44V20h8l8 9.5 8-9.5h8v24h-8V33l-8 9.5L27 33v11z" fill="#07070c"/>
-  <path d="M16 16h32v32H16z" fill="none" stroke="#07070c" stroke-width="3" stroke-linejoin="round"/>
+  <rect width="64" height="64" rx="14" fill="#1c46d6"/>
+  <path d="M14 50V14h9.2l8.8 10.5 8.8-10.5H50v36h-8.2V27.8l-9 10.7-9-10.7V50H14Z" fill="#07070c"/>
 </svg>
 `;
 
